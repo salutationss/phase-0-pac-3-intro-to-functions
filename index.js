09213c601924a3cb99b368368e5093d059376634
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-03-16 20:11:29 -0400

    commit tree 85bd36854309729624c901fa5f940ff81dc86ddb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Follow along with the examples here
+
+
+
+
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2));
+    
+
+num1 = "55"; 
+num2 = "23";
+
+
+  const sum = add(num1, num2);
+const message = `The sum of your numbers is: ${sum}.`;
+
+
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+
+
+  console.log(say("Howdy", "partner"));
